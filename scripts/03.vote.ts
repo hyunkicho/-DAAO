0aa2b544f7407e2a6666173d003c09536e4e6da5
chohyunki <dmsrlgusrl@naver.com>
2024-08-10 22:20:49 +0900

    chore: delete unused code

--- scripts/03.vote.ts
+++ scripts/03.vote.ts
@@ -7,14 +7,14 @@ async function main() {
     const daoAddress = "0x9bee222fdb28f6aedcd9ab349f0c9dcaffae9a97";
     const daaoContract = await ethers.getContractAt("Daao",daoAddress);
 
-    const proposeTx = await daaoContract.propse(
-      [],
-      [],
-      [],
-      "We Should Add AI Governane model in here"
-  );
-  await proposeTx.wait();
-  console.log("proposeTx:", proposeTx.hash);
+  // const proposeTx = await daaoContract.propse(
+  //     [],
+  //     [],
+  //     [],
+  //     "We Should Add AI Governane model in here"
+  // );
+  // await proposeTx.wait();
+  // console.log("proposeTx:", proposeTx.hash);
 
     // const mintTx = await DaaoToken.mint("0x9784054a007F2c61D8Ac7680a81Ff0125a9EC532", "1000000000000000000000");
     // await mintTx.wait();
